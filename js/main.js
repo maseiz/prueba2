4ed69d61059d610f3090a0057c15bbbe0f912d35
Miguel Angel Seiz <mseizduque@gmail.com>
2023-03-07 13:52:22 +0100

    first done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let resultado = document.querySelector("#texto");
+let num1= 0;
+function Sumar(){
+    num1=num1+1;
+    resultado.innerHTML=num1;
+
+}
+function Restar(){
+    num1=num1-1;
+    resultado.innerHTML=num1;
+}
